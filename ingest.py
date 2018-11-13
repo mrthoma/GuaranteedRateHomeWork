3d762403a389bbef543fb659494b94183b5dd272
Mike <35504798+mrthoma@users.noreply.github.com>
2018-11-13 08:15:46 -0600

    Refactor + Error Handling
    More testing revealed a few more issues. To also ensure 80% test coverage and avoid errors from testing, conditionals have been placed through much of the code to catch specific errors. Also refactored variables and cleaned up code a bit.

--- ingest.py
+++ ingest.py
@@ -2,7 +2,13 @@
 ###################################################
 #Guaranteed Rate Dev Homework
 #Mike Thoma 11/11/2018
-
+###################################################
+#EXIT CODES
+#1 UNSPECIFIED/GENERAL FAILURE
+#2 NO ARGUMENT GIVEN
+#3 ALL RECORD PROCESSING FAILED
+#4 NO RECORDS DETECTED
+###################################################
 ###################################################
 #REQUIREMENTS
 ###################################################
@@ -15,7 +21,13 @@ from operator import itemgetter
 ###################################################
 #VARIABLES
 ###################################################
+global 		  plist
+global		  flist
 userfile    = sys.argv[-1]
+#people list
+plist       = []
+#failure list
+flist       = []
 
 ###################################################
 #FUNCTIONS
@@ -23,15 +35,10 @@ userfile    = sys.argv[-1]
 def check_arguments():
 	if len(sys.argv) < 2:
 		print("PLEASE PASS A FILE NAME... EXAMPLE: 'ingest.py test.csv'")
-		exit(1)
+		exit(2)
 
 
 def ingest_input():
-	global 		plist
-	global		flist
-	plist     = []
-	flist     = []
-
 	with open(userfile) as opened:
 		for line in opened:
 
@@ -48,22 +55,43 @@ def ingest_input():
 				flist.append(line.replace('\n', ''))
 
 def print_output():
-	print("ORDERED BY GENDER AND LAST NAME")
-	print(json.dumps(sorted(plist, key=itemgetter('gender', 'lastname'))))
+
+	#if only people list is populated...
+	if plist:
+
+		print("ORDERED BY GENDER AND LAST NAME")
+		print(json.dumps(sorted(plist, key=itemgetter('gender', 'lastname'))))
 
 
-	print("ORDERED BY DOB")
-	print(json.dumps(sorted(plist, key=itemgetter('dob'))))
+		print("ORDERED BY DOB")
+		print(json.dumps(sorted(plist, key=itemgetter('dob'))))
 
 
-	print("REVERSE ORDERED LAST NAME")
-	print(json.dumps(sorted(plist, key=itemgetter('lastname'),reverse = True)))
+		print("REVERSE ORDERED LAST NAME")
+		print(json.dumps(sorted(plist, key=itemgetter('lastname'),reverse = True)))
 
+		#and print failures if we have any
+		if flist:
+			print("FAILED RECORDS")
+			print(flist)
+		#that's all folks
+		exit(0)
 
-	print("FAILED RECORDS")
-	pprint(flist)
+	#otherwise if people list is empty and failure list is populated
+	elif not plist and flist:
+		print("ALL RECORDS FAILED")
+		print(flist)
+		exit(3)
 
+	#otherwise if no people list and no failure list
+	elif not flist and not plist:
+		print("NO RECORDS DETECTED")
+		exit(4)
 
+	#otherwise something else failed
+	else:
+		print("SCRIPT FAILED UKNOWN ERROR...")
+		exit(1)
 
 ###################################################
 #CALLS
